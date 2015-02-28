3be16f1befdf621851404c711fe7151fe341b0a4
Jessica Yuen <jyuen@ualberta.ca>
2015-02-27 20:35:08 -0700

    Finishing assignment, pretty circles and stuff

--- server.py
+++ server.py
@@ -23,7 +23,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, make_response
 import json
 app = Flask(__name__)
 app.debug = True
@@ -76,36 +76,46 @@ def flask_post_json():
         return json.loads(request.form.keys()[0])
 
 
+def json_response(data):
+    response = make_response(json.dumps(data))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
 @app.route("/")
 def hello():
     '''Return something coherent here..
     perhaps redirect to /static/index.html'''
-    return None
+    return redirect('static/index.html')
 
 
 @app.route("/entity/<entity>", methods=['POST', 'PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    for key, value in data.iteritems():
+        myWorld.update(entity, key, value)
+    return json_response(myWorld.get(entity))
 
 
 @app.route("/world", methods=['POST', 'GET'])
 def world():
     '''you should probably return the world here'''
-    return None
+    return json_response(myWorld.world())
 
 
 @app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a
     representation of the entity'''
-    return None
+    return json_response(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST', 'GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json_response(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
